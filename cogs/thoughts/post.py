e3401ad3e5d67333b8d1c0980cfa9d1f683accac
musuke <ultimatemusuke@gmail.com>
2026-01-04 20:38:03 +0900

    Add files via upload

--- cogs/thoughts/post.py
+++ cogs/thoughts/post.py
@@ -3,6 +3,7 @@ from discord import app_commands
 from discord.ext import commands
 from datetime import datetime
 import os
+import traceback
 import sys
 
 # Add the project root to the Python path
