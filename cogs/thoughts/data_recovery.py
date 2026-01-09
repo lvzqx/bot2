e46657e955be910058ebff3a6d5530be6dca99b3
musuke <ultimatemusuke@gmail.com>
2026-01-09 17:03:22 +0900

    Add files via upload

--- cogs/thoughts/data_recovery.py
+++ cogs/thoughts/data_recovery.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 import sqlite3
 import logging
+from typing import Optional
 from bot import DatabaseMixin
 
 logger = logging.getLogger(__name__)
